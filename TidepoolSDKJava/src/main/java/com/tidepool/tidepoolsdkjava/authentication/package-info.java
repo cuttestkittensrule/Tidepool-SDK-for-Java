400b79b3b037aa71a32eab81a00288441f590533
cuttestkittensrule <mangoiscute95@gmail.com>
2023-05-18 08:47:20 -0700

    Authentication docs updated

--- TidepoolSDKJava/src/main/java/com/tidepool/tidepoolsdkjava/authentication/package-info.java
+++ TidepoolSDKJava/src/main/java/com/tidepool/tidepoolsdkjava/authentication/package-info.java
@@ -1,4 +1,7 @@
+/*
+	TODO: If and/or when the current (4/25/2023) Authentication category is deprecated, and removed from the docs, replace with new name
+*/
 /**
- * Everything in the authentication category in the <a href="https://tidepool.stoplight.io/docs/tidepool-full-api">stoplight docs</a>
+ * Everything in the authentication v2 category in the <a href="https://tidepool.stoplight.io/docs/tidepool-full-api">stoplight docs</a>
  */
 package com.tidepool.tidepoolsdkjava.authentication;
